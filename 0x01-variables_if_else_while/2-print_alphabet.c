942058d74430e3870702f498c9a6ee05946e92a8
Lwenzile Maseko <masekolwenzile@gmail.com>
2023-04-23 01:50:54 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,16 +7,15 @@
 
 int main(void)
 {
-	char c;
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	c = 'a';
-	while
-		(c <= 'z') {
-		       	putchar(c);
-			c++;
-		}
+	for (i = 0; i < 26; i++)
+	{
+		putcharr(alp[i]);
+	}
 	putchar('\n');
 	return (0);
 }
-	
- 
+
+

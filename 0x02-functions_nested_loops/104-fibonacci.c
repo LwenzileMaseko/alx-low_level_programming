559f1bae04ef90eeb8323f001a90fd98b03dc9d2
Lwenzile Maseko <masekolwenzile@gmail.com>
2023-04-26 11:39:34 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,7 +14,7 @@ int main(void)
 
 	for (count = 0; count < 92; count++)
 	{
-	sum = fib1 + fib2
+	sum = fib1 + fib2;
 	printf("%lu, ", sum);
 	fib1 = fib2;
 	fib2 = sum;
@@ -25,6 +25,10 @@ int main(void)
 	fib2_half2 = fib2 % 10000000000;
 	for (count = 93; count < 99; count++)
 	{
+	half1 = fib1_half1 + fib2_half1;
+	half2 = fib1_half2 + fib2_fib2;
+	if (fib1_half2 + fib2_half2 > 9999999999)
+	{	
 	half1 += 1;
 	half2 %= 10000000000;
 	}
